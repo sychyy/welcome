4bebc250a24d6644b80b4c6d54e95d30a28e8889
sychyy <ydaanewww@gmail.com>
2025-02-09 22:00:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 
 // Menangani permintaan ke /welcome
 app.get('/welcome', async (req, res) => {
-    const background = req.query.background || 'https://cdn.popcat.xyz/welcome-bg.png';
-    const text1 = req.query.text1 || 'Zero Two';
-    const text2 = req.query.text2 || 'Welcome To Pop Cat Community';
-    const text3 = req.query.text3 || 'Member 219';
-    const avatar = req.query.avatar || 'https://cdn.discordapp.com/embed/avatars/0.png';
+    const background = req.query.background || 'https://files.catbox.moe/lh40j5.png';
+    const text1 = req.query.text1 || 'Sycze';
+    const text2 = req.query.text2 || 'Welcome To Sycze-Team';
+    const text3 = req.query.text3 || 'Member 1986';
+    const avatar = req.query.avatar || 'https://files.catbox.moe/316j0f.jpg';
 
     // Membentuk URL Popcat dengan parameter yang diterima
     const popcatUrl = `https://api.popcat.xyz/welcomecard?background=${encodeURIComponent(background)}&text1=${encodeURIComponent(text1)}&text2=${encodeURIComponent(text2)}&text3=${encodeURIComponent(text3)}&avatar=${encodeURIComponent(avatar)}`;
